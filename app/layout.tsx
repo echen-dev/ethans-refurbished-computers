26f1f8d85098897d981f90c29b6fed1b09fd8123
Ethan Chen <Ethanchen4@gmail.com>
2026-02-06 19:02:27 -0500

    Adding loading and not found pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
