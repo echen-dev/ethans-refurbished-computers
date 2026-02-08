bba041fc83a4792a6bf305865096516f8d97686b
Ethan Chen <Ethanchen4@gmail.com>
2026-02-07 23:29:08 -0500

    added sample data and a product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Homepage = () => {
-  return <>Ethan's Refurbished Computers</>;
+  console.log(sampleData);
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
---
+++ db/sample-data.ts
@@ -0,0 +1,95 @@
+const sampleData = {
+  products: [
+    {
+      name: "Dell Precision T1700 Workstation",
+      slug: "dell-precision-t1700-workstation",
+      category: "Refurbished Desktop",
+      description:
+        "Computer has been cleaned and tested with minor scratches on case",
+      images: [
+        "/images/sample-products/dell-workstation.webp",
+        "/images/sample-products/dell-workstation-2.webp",
+      ],
+      price: 109.99,
+      brand: "Dell",
+      rating: 4.5,
+      numReviews: 10,
+      stock: 1,
+      isFeatured: true,
+      banner: "banner-1.jpg",
+    },
+    {
+      name: "Lenovo ThinkCentre M920s",
+      slug: "lenovo-thinkcentre-m920s",
+      category: "Refurbished Desktop",
+      description: "Computer is like-new and back by a one year warranty",
+      images: [
+        "/images/sample-products/lenovo-thinkcentre.webp",
+        "/images/sample-products/lenovo-thinkcentre-2.webp",
+      ],
+      price: 155.95,
+      brand: "Lenovo",
+      rating: 4.2,
+      numReviews: 8,
+      stock: 1,
+      isFeatured: true,
+      banner: "banner-2.jpg",
+    },
+    {
+      name: "HP EliteDesk 800 G5",
+      slug: "hp-elitedesk-800-g5",
+      category: "Refurbished Desktop",
+      description:
+        "Professionally refurbished and extensively tested, comes with a copy of Windows 11",
+      images: [
+        "/images/sample-products/hp-elitedesk.webp",
+        "/images/sample-products/hp-elitedesk-2.webp",
+      ],
+      price: 199.95,
+      brand: "HP",
+      rating: 4.9,
+      numReviews: 3,
+      stock: 1,
+      isFeatured: false,
+      banner: null,
+    },
+    {
+      name: "Dell Latitude 5400 ChromeBook 14 Inch",
+      slug: "dell-latitude-5400-chromebook-14-inch",
+      category: "Refurbished Laptop",
+      description:
+        "Item may have some scratches due to use, comes with charger",
+      images: [
+        "/images/sample-products/dell-latitude.webp",
+        "/images/sample-products/dell-latitude-2.webp",
+      ],
+      price: 95.5,
+      brand: "Dell",
+      rating: 4.9,
+      numReviews: 3,
+      stock: 1,
+      isFeatured: false,
+      banner: null,
+    },
+    {
+      name: "Lenovo ThinkPad X390",
+      slug: "dell-latitude-5400-chromebook-14-inch",
+      category: "Refurbished Laptop",
+      description:
+        "Has signs of comestic wear including scratches from previous use, comes with charger",
+      images: [
+        "/images/sample-products/lenovo-x390.webp",
+        "/images/sample-products/lenovo-x390-2.webp",
+      ],
+      price: 75.0,
+      brand: "Lenovo",
+      rating: 4.9,
+      numReviews: 3,
+      stock: 1,
+      isFeatured: false,
+      banner: null,
+    },
+  ],
+};
+
+export default sampleData;
