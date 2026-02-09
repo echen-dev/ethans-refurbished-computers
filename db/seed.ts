b81744f19277fa5a4842c876f418cb16a26af75f
Ethan Chen <Ethanchen4@gmail.com>
2026-02-09 02:25:46 -0500

    Adding sample user data

--- db/seed.ts
+++ db/seed.ts
@@ -8,8 +8,15 @@ async function main() {
   const connectionString = `${process.env.DATABASE_URL}`;
   const adapter = new PrismaNeon({ connectionString });
   const prisma = new PrismaClient({ adapter });
+
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
   console.log("Database seeded successfully");
 }
 
