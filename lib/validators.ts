88c99e9090ac3b6f333970b297b4ee0db21e9d78
Ethan Chen <Ethanchen4@gmail.com>
2026-02-10 17:41:57 -0500

    Adding sign up user action

--- lib/validators.ts
+++ lib/validators.ts
@@ -28,3 +28,18 @@ export const signInFormSchema = z.object({
   email: z.email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 8 characters"),
 });
+
+// Sign Up Schema
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be at least 3 characters"),
+    email: z.email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 8 characters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm Password must be at least 8 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
