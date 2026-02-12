0700b26fd909b44f5f09f1ee48ecec6c5560044e
Ethan Chen <Ethanchen4@gmail.com>
2026-02-12 00:31:20 -0500

    Adding UI for removing cart items from product detail page

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { CartItem } from "@/types";
+import { Cart, CartItem } from "@/types";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
+import { Minus, Plus } from "lucide-react";
 import { toast } from "sonner";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
   const handleAddToCart = async () => {
     const res = await addItemToCart(item);
@@ -23,7 +23,36 @@ const AddToCart = ({ item }: { item: CartItem }) => {
       },
     });
   };
-  return (
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+    if (!res.success) {
+      toast.error("Something went wrong!", {
+        description: res.message,
+      });
+      return;
+    }
+    toast.success(res.message, {
+      action: {
+        label: "Go To Cart",
+        onClick: () => router.push("/cart"),
+      },
+    });
+    return;
+  };
+
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="h-4 w-4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="h-4 w-4" />
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
       <Plus /> Add to Cart
     </Button>
--- lib/validators.ts
+++ lib/validators.ts
@@ -56,6 +56,6 @@ export const insertCartSchema = z.object({
   totalPrice: currency,
   shippingPrice: currency,
   taxPrice: currency,
-  sessionCartId: z.string().min(1, "Session Cart ID is required"),
-  userId: z.string().optional(),
+  sessionCartId: z.string().min(1, "Session cart id is required"),
+  userId: z.string().optional().nullable(),
 });
