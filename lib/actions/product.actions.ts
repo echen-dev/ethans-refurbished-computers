611d76f5b6f648be9803695b872ae42face00275
Ethan Chen <Ethanchen4@gmail.com>
2026-02-23 14:06:58 -0500

    Adding update product schema and actions for creating and deleting products

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,6 +3,8 @@ import { prisma } from "@/db/prisma";
 import { convertToPlainObject, formatError } from "../utils";
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
+import { insertProductSchema, updateProductSchema } from "../validators";
+import { z } from "zod";
 
 export async function getLatestProducts() {
   const data = await prisma.product.findMany({
@@ -50,9 +52,41 @@ export async function deleteProduct(id: string) {
     revalidatePath("/admin/products");
     return { success: true, message: "Product deleted successfully" };
   } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+export async function createProduct(data: z.infer<typeof insertProductSchema>) {
+  try {
+    const product = insertProductSchema.parse(data);
+    await prisma.product.create({ data: product });
+    revalidatePath("/admin/products");
     return {
-      success: false,
-      message: formatError(error),
+      success: true,
+      message: "Product created successfully",
     };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
+  try {
+    const product = updateProductSchema.parse(data);
+    const productExists = await prisma.product.findFirst({
+      where: { id: product.id },
+    });
+    if (!productExists) throw new Error("Product not found");
+    await prisma.product.update({
+      where: { id: product.id },
+      data: product,
+    });
+    revalidatePath("/admin/products");
+    return {
+      success: true,
+      message: "Product updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
   }
 }
